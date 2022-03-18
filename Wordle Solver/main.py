e60297c1864d0ca1f84989b15d6d07d5bd1cc38e
jasonhn10 <jasonhn10@gmail.com>
2022-03-18 15:50:32 -0600

    Wordle Solver

--- Wordle Solver/main.py
+++ Wordle Solver/main.py
@@ -1,6 +1,13 @@
 import csv
 
-#CSV to array
+# Jason Niemann's Wordle Solver
+# Wordle website: https://www.nytimes.com/games/wordle/index.html
+
+# A simple program to solve Wordles. 'words.csv' contains a list of 330k+ English words sorted from most to least
+# common. The file is read in and the words are stored in an array. The if statement on line 64 can be used to
+# deduce which words fit the Wordle criteria.
+
+# CSV to array
 file = open("words.csv", "r")
 reader = csv.reader(file)
 words = []
@@ -8,7 +15,7 @@ for row in reader:
     words.append(row[0])
 
 
-# Is 5 Letters
+# Is x Letters
 def hasLength(length):
     if len(word) == length:
         return True
@@ -58,19 +65,28 @@ def doesNotContain(letters):
 # Find possible words
 possibleWords = []
 for word in words:
+    # TODO: Use the doesNotContain, ___atPos, and notAtPos functions to solve your Wordle
+    # Place multiple letters between the quotation marks as necessary: ___atPos('hello')
+    # Be careful with double letters. For example, if your last guess was 'plaza' and the 'a' at position 3
+    # is correct, but the 'a' at position 5 is incorrect, then you should mark ___atPos(3, 'a') and notAtPos(5, 'a').
+    # Do NOT mark doesNotContain('a') as this would contradict the fact that ___atPos(3, 'a') is true.
+    # Double check that your letters are in the appropriate spots!
+    # Enjoy!
     if hasLength(5) and doesNotContain('') and contains('') and \
             ___atPos(1, '') and ___atPos(2, '') and ___atPos(3, '') and ___atPos(4, '') and ___atPos(5, '') and \
             notAtPos(1, '') and notAtPos(2, '') and notAtPos(3, '') and notAtPos(4, '') and notAtPos(5, ''):
         possibleWords.append(word)
 
-
-# Display next best word
+# Display next best word and all possible words
 print("All possible words (ordered by likelihood):")
 for word in possibleWords:
     print(word)
 print()
 print(str(len(possibleWords)) + " possible words")
 if len(possibleWords) == 39933:
+    # According to various online sources, 'roate' is statistically the best first word.
     print("Next suggested word: roate")
 else:
     print("Next suggested word: " + possibleWords[0])
+
+# Future Project: Incorporate program into Google Chrome Extension. Start with 'manifest.json' file.
